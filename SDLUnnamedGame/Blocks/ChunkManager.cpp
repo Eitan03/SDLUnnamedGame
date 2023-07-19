2d738eedad14b3b3e022c65890f50535036add26
Eitan03 <eitanabcabc@gmail.com>
2023-07-19 20:38:34 +0300

    improved line reading in chunk loading

--- SDLUnnamedGame/Blocks/ChunkManager.cpp
+++ SDLUnnamedGame/Blocks/ChunkManager.cpp
@@ -46,7 +46,8 @@ std::set<MGL::PointI> ChunkManager::getChunksToLoad(MGL::PointI cameraChunkPosit
 	std::set<MGL::PointI> chunksToLoad;
 	
 
-	MGL::PointI screenSizeInChunks = MGL::PointI( (SCREEN_WIDTH / Block::getSizeScaled()) / CHUNK_SIZE, (SCREEN_HEIGHT / Block::getSizeScaled()) / CHUNK_SIZE ) + 2;
+	// MGL::PointI screenSizeInChunks = MGL::PointI( (SCREEN_WIDTH / Block::getSizeScaled()) / CHUNK_SIZE, (SCREEN_HEIGHT / Block::getSizeScaled()) / CHUNK_SIZE ) + 2;
+	MGL::PointI screenSizeInChunks = MGL::PointI( SCREEN_WIDTH / (Block::getSizeScaled() * CHUNK_SIZE), SCREEN_HEIGHT / (Block::getSizeScaled() * CHUNK_SIZE)) + 2;
 
 	for (MGL::PointI chunkPosition = cameraChunkPosition; chunkPosition.x < cameraChunkPosition.x + screenSizeInChunks.x; (chunkPosition.x)++) {
 		for (; chunkPosition.y < cameraChunkPosition.y + screenSizeInChunks.y;  (chunkPosition.y)++ ) {
@@ -84,13 +85,6 @@ void ChunkManager::setBlock(std::unique_ptr<Block> block, int layer, MGL::PointI
 {
 	MGL::PointI chunkPos = MGL::PointI{ (int)std::floor(position.x / (CHUNK_SIZE * 1.0)), (int)std::floor(position.y / (CHUNK_SIZE * 1.0)) };
 	MGL::PointI blockPos = position - (chunkPos * CHUNK_SIZE);
-	/*
-	if (chunkPos.x < 0) {
-		blockPos.x += chunkPos.x;
-	}
-	if (chunkPos.y < 0) {
-		blockPos.y += chunkPos.y;
-	}*/
 	if (loadedChunks.find(chunkPos) == loadedChunks.end()) {
 		loadChunk(chunkPos);
 	}
